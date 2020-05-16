5b10b8fba4321b20c32f95a952729816c17411b6
Shyam Sundhar <clashwithchiefrpjyt@gmail.com>
2020-05-17 02:35:00 +0530

    Show Jobs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'shyam1411',
+        'HOST':'localhost',
+        'PORT':'5432',
+
     }
 }
 
